633c6ba5e5781bc31caf17dc564034a81c178de1
David Boedigheimer <stealth259@gmail.com>
2019-09-10 13:58:11 -0700

    Add remove functions on image click.

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,15 @@ $(document).ready(function() {
 
   $("h2#human").click(function() {
     $('h2#human').after('<img src="img/humans.jpg" alt="A photo of the humans." width="350px">')
+    $("img").click(function() {
+      $(this).remove();
+    });
   });
 
   $("h2#atealen").click(function() {
     $('h2#atealen').after('<img src="img/atealens.jpg" alt="A photo of the Atealens." width="350px">')
+    $("img").click(function() {
+      $(this).remove();
+    });
   });
 });
